33bc24089c481492eec478b4a9a964fd448dd2fe
shawn <shawn@mobiata.com>
2013-02-27 16:51:46 -0500

    Correctly handling redirects that contain cookies

--- lib/httpsimple.rb
+++ lib/httpsimple.rb
@@ -1,8 +1,9 @@
 require 'net/http'
 require 'uri'
+require 'cgi'
 
 module HttpSimple
-  VERSION='1.0.4'
+  VERSION='1.0.5'
   def self.get(url, data=nil, &block)
     request(url, :get, data, &block)
   end
@@ -46,6 +47,7 @@ module HttpSimple
       @debug = false
       # Response handlers
       @handlers = {}
+      @set_cookie_headers = []
                   
     end
     
@@ -93,54 +95,91 @@ module HttpSimple
       
     end
      
-    def fetch(uri, request, limit=1)                
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = uri.scheme == 'https'
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE unless @scrict_ssl
-      http.set_debug_output $stderr if @debug
-      http.read_timeout = @timeout
+    private 
+     
+      def fetch(uri, request, limit=1)                
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = uri.scheme == 'https'
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE unless @scrict_ssl
+        http.set_debug_output $stderr if @debug
+        http.read_timeout = @timeout
       
-      @headers.each_pair { |k,v| request[k] = v } unless @headers.empty?
+        @headers.each_pair { |k,v| request[k] = v } unless @headers.empty?
             
-      response = http.start do |http|
-        http.request(request)
-      end
-      
-      code = response.code.to_sym
-      if @handlers.key?(code)
-        @handlers[code].call(http, request, response)
-      end
-              
-      case response
-      when Net::HTTPSuccess
-        return response
-      when Net::HTTPRedirection
-        return response unless @follow_redirects
-        raise HTTPMaxRedirectError.new(@max_redirects) if limit == 0        
-        block = lambda { |url, req| fetch(url, req, limit - 1) }
-        new_uri = URI(response['location'])
-        # Handle relative redirects ie /foo        
-        new_uri = uri + new_uri unless new_uri.is_a? URI::HTTP
-        if request.is_a? Net::HTTP::Get
-          get(new_uri, &block) 
-        elsif request.is_a? Net::HTTP::Post
-          post(new_uri, &block)
+        response = http.start do |http|
+          http.request(request)
         end
-      when Net::HTTPResponse
-        response.error!
+      
+        code = response.code.to_sym
+        if @handlers.key?(code)
+          @handlers[code].call(http, request, response)
+        end
+              
+        case response
+        when Net::HTTPSuccess
+          return update_response_cookies(response)
+        when Net::HTTPRedirection
+          return response unless @follow_redirects
+          raise_error_if_redirection_limit_reached(limit)
+          new_uri = URI(response['location'])
+          # Handle relative redirects ie /foo        
+          new_uri = uri + new_uri unless new_uri.is_a? URI::HTTP
+          merge_request_cookies(get_cookies_for_redirect(response))
+          block = lambda { |url, req| fetch(url, req, limit - 1) }
+                
+          if request.is_a? Net::HTTP::Get
+            get(new_uri, &block) 
+          elsif request.is_a? Net::HTTP::Post
+            post(new_uri, &block)
+          end
+        when Net::HTTPResponse
+          response.error!
+        end
+      
+      end
+    
+      def raise_error_if_redirection_limit_reached(limit)
+        raise HTTPMaxRedirectError.new(@max_redirects) if limit == 0
+      end    
+            
+      def get_cookies_for_redirect(response)
+        cookies = response.get_fields("set-cookie")
+        return if cookies.nil?        
+        @set_cookie_headers.concat(cookies)
+        
+        cookies.collect! {|raw_cookie|
+          name, value = get_cookie_name_value_pair(raw_cookie)
+          "#{name}=#{value}" unless value.nil?
+        }.compact!
+        
+        cookies.join("; ") unless cookies.empty?        
       end
       
-    end
-    private :fetch
-    
-    def get_path(uri)
-      if uri.path.length == 0 and uri.query.nil?
-        "/"
-      elsif uri.query.nil?
-        uri.path
-      else
-        "#{uri.path}?#{uri.query}"
+      def get_cookie_name_value_pair(raw_cookie)
+        ignore = ["domain", "path", "expires"]
+        CGI::Cookie::parse(raw_cookie).each_pair { |k,v|
+          return k,v[0] unless ignore.include? k.downcase
+        }
+        raise ArgumentError.new("Invalid set-cookie value: #{raw_cookie}")  
+      end 
+      
+      def merge_request_cookies(cookies)
+        # TODO: scrutinize this merging logic. Duplicate cookies from
+        # ``cookies`` should probably overwrite the ones in ``@headers``.
+        return unless !cookies.nil?
+        if @headers.include? "cookie"
+          @headers['cookie'] = [@headers['cookie'], cookies].join("; ")
+        else
+          @headers['cookie'] = cookies
+        end
       end
-    end
+      
+      def update_response_cookies(response)
+        @set_cookie_headers.each { |cookie|
+          response.header.add_field('set-cookie', cookie)
+        }
+        @set_cookie_headers = []
+        response
+      end                  
   end    
 end
